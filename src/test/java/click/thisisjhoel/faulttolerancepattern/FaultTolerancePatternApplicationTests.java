dcdcdc16581ac2c816aeaf0622022bc3762bc83f
Jhoel Galeano Bagnasco <bagnascojhoel@gmail.com>
2022-04-16 19:13:43 -0300

    initial commit

---
+++ src/test/java/click/thisisjhoel/faulttolerancepattern/FaultTolerancePatternApplicationTests.java
@@ -0,0 +1,13 @@
+package click.thisisjhoel.faulttolerancepattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaultTolerancePatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
